825c823dff50174f44ce039784a1bf9539e0da91
Katrina Ezis <katrinaezis@gmail.com>
2015-10-26 14:20:29 -0700

    finished, lerl not really

--- js/main.js
+++ js/main.js
@@ -23,13 +23,26 @@
 
 $('button').click(function() {
     var thisButton = $(this).attr('id');
+    var selector = $("#selector").val();
+    var apply = $("#newContent").val();
     if (thisButton == "change-content") {
-        var selector = $("#selector").val();
-        var apply = $("#newContent").val();
-        $(selector).val() = apply;
+        console.log(selector);
+        $(selector).html(apply);
     } else if (thisButton == "add-at-end") {
-        var selector = $("#selector").val();
-        var apply = $("#newContent").val();
-        
+        $(selector).append("<div>" + apply + "</div>");
+    } else if (thisButton == "add-at-start") {
+        $(selector).prepend("<div>" + apply + "</div>");
+    } else if (thisButton == "insert-before") {
+        $("<div>" + apply + "</div>").insertBefore(selector);
+    } else if (thisButton == "move-after") {
+        $()
+    } else if (thisButton == "surround-class") {
+
+    } else if (thisButton == "hide-text") {
+
+    } else if (thisButton == "remove-word") {
+        if ($(selector).contains(apply)) {
+            $(selector).remove();
+        }
     }
 });
