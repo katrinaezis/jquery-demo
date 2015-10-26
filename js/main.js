33f76ba3360dc43fd9d32b5c27c03853fe31b5a4
Katrina Ezis <katrinaezis@gmail.com>
2015-10-26 09:37:03 -0700

    did nothing

--- js/main.js
+++ js/main.js
@@ -2,3 +2,34 @@
 
 /* Use clever selections and event handlers to assign the described interactivity */
 
+
+
+
+
+// $('#change-content').click(function() {
+//     var selector = $("#selector").val();
+//     var apply = $("#newContent").val();
+//     console.log(selector);
+//     selector.val() = apply;
+// }); 
+
+// $('#add-at-end').click(function() {
+//     alert('kk');
+//     $('#selector').append('<div>' + apply + '</div>');
+// });
+
+
+
+
+$('button').click(function() {
+    var thisButton = $(this).attr('id');
+    if (thisButton == "change-content") {
+        var selector = $("#selector").val();
+        var apply = $("#newContent").val();
+        $(selector).val() = apply;
+    } else if (thisButton == "add-at-end") {
+        var selector = $("#selector").val();
+        var apply = $("#newContent").val();
+        
+    }
+});
